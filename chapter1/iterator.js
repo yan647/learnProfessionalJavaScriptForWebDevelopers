1f2553fd132415ef844028c10c22df20d2d5d8a5
刘水清 <yan647@aliyun.com>
2022-04-10 21:04:41 +0800

    feat(iterator): 支持创建多个迭代器

--- chapter1/iterator.js
+++ chapter1/iterator.js
@@ -1,33 +1,32 @@
-/**
- * Created by lsq on 2022/4/10.
- */
-
-'use strict';
-
 class Counter {
     constructor(limit) {
-        this.count = 1;
         this.limit = limit;
     }
 
-    next() {
-        if (this.count < this.limit) {
-            return {
-                done: false, value: this.count++,
-            }
-        } else {
-            return {
-                done: true, value: undefined,
-            }
-        }
-    }
-
+    // 迭代器工厂函数，返回一个新迭代器
     [Symbol.iterator]() {
-        return this;
+        let count = 1, limit = this.limit;
+        return {// 使用闭包，支持可迭代对象创建多个迭代器
+            next() {
+                if (count < limit) {
+                    return {// 迭代器对象
+                        done: false, value: count++,
+                    }
+                } else {
+                    return {
+                        done: true, value: undefined,
+                    }
+                }
+            }
+        };
     }
 }
 
 let counter = new Counter(4);
+
+for (let a of counter) {
+    console.log(a);
+}
 
 for (let a of counter) {
     console.log(a);
